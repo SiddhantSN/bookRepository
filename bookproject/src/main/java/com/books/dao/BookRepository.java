60044bae2d10b9365dcadbd7afa6dc336d96cb6a
siddh <siddh@DESKTOP-PVH6BSP>
2023-09-25 23:18:25 +0530

    Removed year functionality.

--- bookproject/src/main/java/com/books/dao/BookRepository.java
+++ bookproject/src/main/java/com/books/dao/BookRepository.java
@@ -3,11 +3,13 @@ package com.books.dao;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.books.entities.Book;
 
 public interface BookRepository extends JpaRepository<Book, String> {
 
-	public List<Book> findByyearOfPublication(int publication);
+//	@Query(value = "SELECT * FROM books.book b where b.yearOfPublication = ?1", nativeQuery = true)
+//	public List<Book> getBooksByYearOfPublication(int yearOfPublication);
 	
 }
