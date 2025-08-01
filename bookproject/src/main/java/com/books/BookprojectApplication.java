1b394d8da92640790746f4c65ee21a8a03842de8
Siddhant <sid5878@gmail.com>
2025-08-01 12:15:30 +0530

    Imports Organized

--- bookproject/src/main/java/com/books/BookprojectApplication.java
+++ bookproject/src/main/java/com/books/BookprojectApplication.java
@@ -1,11 +1,5 @@
 package com.books;
 
-import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.StreamSupport;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
--- bookproject/src/main/java/com/books/entities/User.java
+++ bookproject/src/main/java/com/books/entities/User.java
@@ -3,14 +3,10 @@ package com.books.entities;
 import java.util.ArrayList;
 import java.util.List;
 
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToMany;
-import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
